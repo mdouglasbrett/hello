881873214f63a090016149f3cc0f85e282a74172
Michael Brett <mdouglasbrett@icloud.com>
2023-07-03 19:39:33 +0100

    Uses ThreadPool
    Initial commit as part of the "compiler driven design" strategy.
    This code will not compile, but we will use the compiler to help us
    flesh out the functionality that we want to achieve.

--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,14 @@ use std::{
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 }
 
@@ -27,13 +30,13 @@ fn handle_connection(mut stream: TcpStream) {
         // handle the error from the line read
         .unwrap();
 
-    let (status_line, filename) = match &request_line [..] {
+    let (status_line, filename) = match &request_line[..] {
         "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
         "GET /sleep HTTP/1.1" => {
             thread::sleep(Duration::from_secs(5));
             ("HTTP/1.1 200 OK", "hello.html")
         }
-        _ => ("HTTP/1.1 404 NOT FOUND", "404.html")
+        _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
     };
 
     let contents = fs::read_to_string(filename).unwrap();
