41b2c2bcc1d559179f57c9d2c71a0d94e80619ba
Michael Brett <mdouglasbrett@icloud.com>
2023-07-03 19:15:09 +0100

    Adds not found handling

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::{
-    fs,
+    fs::{self, read_to_string},
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
 };
@@ -16,12 +16,13 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
-    let request_line = buf_reader.lines()
+    let request_line = buf_reader
+        .lines()
         // move the iterator
         .next()
         // handle the option if no items
         .unwrap()
-        // handle the error from the line read 
+        // handle the error from the line read
         .unwrap();
 
     if request_line == "GET / HTTP/1.1" {
@@ -36,5 +37,16 @@ fn handle_connection(mut stream: TcpStream) {
 
         stream.write_all(response.as_bytes()).unwrap();
     } else {
+        let status_line = "HTTP/1.1 404 NOT FOUND";
+        let contents = fs::read_to_string("404.html").unwrap();
+        let length = contents.len();
+
+        let response = format!(
+            "{status_line}\r\n\
+        Content-Length: {length}\r\n\r\n\
+        {contents}"
+        );
+
+        stream.write_all(response.as_bytes()).unwrap();
     }
 }
